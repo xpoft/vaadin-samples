1b6b6309a46480272c06e3232d669ee260ba59f2
Alexander Fedorov <xpoft@xpoft.ru>
2012-10-29 16:33:14 +0400

    catch errors

--- apache-shiro/src/main/java/ru/xpoft/vaadin/apache_shiro_sample/MyUI.java
+++ apache-shiro/src/main/java/ru/xpoft/vaadin/apache_shiro_sample/MyUI.java
@@ -1,9 +1,11 @@
 package ru.xpoft.vaadin.apache_shiro_sample;
 
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.UI;
+import com.vaadin.server.*;
+import com.vaadin.ui.*;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import ru.xpoft.vaadin.DiscoveryNavigator;
 import ru.xpoft.vaadin.security.ShiroSecurityNavigator;
 
 /**
@@ -11,14 +13,58 @@ import ru.xpoft.vaadin.security.ShiroSecurityNavigator;
  */
 @Component
 @Scope("prototype")
-public class MyUI extends UI
+public class MyUI extends UI implements Terminal.ErrorListener
 {
     @Override
     protected void init(final VaadinRequest request)
     {
+        VaadinServiceSession.getCurrent().setErrorHandler(this);
         setSizeFull();
 
-        ShiroSecurityNavigator navigator = new ShiroSecurityNavigator(this, getContent());
-        navigator.navigateTo(UI.getCurrent().getPage().getFragment());
+        try
+        {
+            ShiroSecurityNavigator navigator = new ShiroSecurityNavigator(this, getContent());
+            navigator.navigateTo(UI.getCurrent().getPage().getFragment());
+        }
+        /**
+         * Exception on page load
+         */
+        catch (IllegalArgumentException e)
+        {
+            Label label = new Label(e.getMessage());
+            label.setWidth(-1, Unit.PERCENTAGE);
+
+            Link goToMain = new Link("Go to main", new ExternalResource("/"));
+
+            VerticalLayout layout = new VerticalLayout();
+            layout.addComponent(label);
+            layout.addComponent(goToMain);
+            layout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+            layout.setComponentAlignment(goToMain, Alignment.MIDDLE_CENTER);
+
+            VerticalLayout mainLayout = new VerticalLayout();
+            mainLayout.setSizeFull();
+            mainLayout.addComponent(layout);
+            mainLayout.setComponentAlignment(layout, Alignment.MIDDLE_CENTER);
+
+            setContent(mainLayout);
+            Notification.show(e.getMessage(), Notification.Type.ERROR_MESSAGE);
+        }
+    }
+
+    /**
+     * Exception on action
+     */
+    @Override
+    public void terminalError(Terminal.ErrorEvent event)
+    {
+        if (event.getThrowable().getCause() instanceof IllegalArgumentException)
+        {
+            IllegalArgumentException exception = (IllegalArgumentException) event.getThrowable().getCause();
+            Notification.show(exception.getMessage(), Notification.Type.ERROR_MESSAGE);
+            return;
+        }
+
+        DefaultErrorListener.doDefault(event);
     }
 }
--- apache-shiro/src/main/java/ru/xpoft/vaadin/apache_shiro_sample/web/LoginController.java
+++ apache-shiro/src/main/java/ru/xpoft/vaadin/apache_shiro_sample/web/LoginController.java
@@ -1,14 +1,9 @@
 package ru.xpoft.vaadin.apache_shiro_sample.web;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.*;
-import org.apache.shiro.subject.Subject;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,6 +17,29 @@ public class LoginController
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String login()
     {
+        return "login";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String loginForm(HttpServletRequest request, ModelMap modelMap)
+    {
+        if (request.getAttribute("shiroLoginFailure") != null)
+        {
+            String exception = (String) request.getAttribute("shiroLoginFailure");
+            if (exception.endsWith("UnknownAccountException") || exception.endsWith("IncorrectCredentialsException"))
+            {
+                modelMap.put("userNotFound", true);
+            }
+            else if (exception.endsWith("LockedAccountException"))
+            {
+                modelMap.put("userLocked", true);
+            }
+            else
+            {
+                modelMap.put("error", true);
+            }
+        }
+
         return "login";
     }
 }
